4ba8b2c267af3166d4e2b5179f78ac75c8f5a73c
cdimitroulas <c.dimitroulas@gmail.com>
2023-12-04 12:59:32 +0000

    Refactored to use Sets instead of lists

--- src/Solutions/Day04.hs
+++ src/Solutions/Day04.hs
@@ -6,12 +6,14 @@ import Lib.AOC (runSolution)
 import Lib.Parsing (linesOf)
 import Data.Map.Strict (Map)
 import qualified Data.Map as M
+import qualified Data.Set as S
 import Data.Foldable (foldl')
+import Data.Set (Set)
 
 day04 :: IO ()
 day04 = runSolution "04" parser (fmap part1) (fmap part2)
 
-data Card = Card {cardNum :: Int, winningNums :: [Int], playerNums :: [Int], copies :: Int} deriving (Show)
+data Card = Card {cardNum :: Int, winningNums :: Set Int, playerNums :: Set Int, copies :: Int} deriving (Show)
 
 type Input = [Card]
 
@@ -21,7 +23,7 @@ parser = P.parseOnly (linesOf cardParser) . T.pack
 cardParser :: P.Parser Card
 cardParser =
   let numParser = read <$> (P.string "Card" *> P.many1 P.space *> P.many1 P.digit <* P.char ':')
-      numberParser = P.many1 P.space *> (map read <$>  P.many1 P.digit `P.sepBy` P.many1 (P.char ' '))
+      numberParser = S.fromList <$> (P.many1 P.space *> (map read <$>  P.many1 P.digit `P.sepBy` P.many1 (P.char ' ')))
    in Card <$> numParser <*> numberParser <* P.string " |" <*> numberParser <*> pure 1
 
 part1 :: Input -> Int
@@ -31,7 +33,7 @@ part1 = sum . map cardPoints
     cardPoints card = if totalWinning card == 0 then 0 else 2 ^ (totalWinning card - 1)
 
 totalWinning :: Card -> Int
-totalWinning card = length $ filter (`elem` card.playerNums) card.winningNums
+totalWinning card = length $ S.intersection card.winningNums card.playerNums
 
 part2 :: Input -> Int
 part2 input = M.foldl' (\total card -> total + card.copies) 0 $ createCopies (cardNum $ head input) $ toMap input
