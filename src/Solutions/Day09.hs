e05aee015cc24910115aaa6e2ce0b93764fb449d
cdimitroulas <c.dimitroulas@gmail.com>
2023-12-09 20:40:26 +0000

    Refactored day9

--- src/Solutions/Day09.hs
+++ src/Solutions/Day09.hs
@@ -15,24 +15,25 @@ differences [] = []
 differences [_] = []
 differences (x1:x2:xs) = x2 - x1 : differences (x2:xs)
 
-extrapolateSequence :: ExtrapolateDir -> [Int] -> [Int]
-extrapolateSequence dir xs = if dir == Forward then xs <> [go [] xs] else go [] xs : xs
+-- Returns the next/previous element for a sequence
+extrapolateSequence1 :: ExtrapolateDir -> [Int] -> Int
+extrapolateSequence1 dir xs = go [] xs
     where
       combiner = if dir == Forward then (+) else (-)
       accessor = if dir == Forward then last else head
       foldFn = foldl' (flip combiner) 0
 
-      go :: [[Int]] -> [Int] -> Int
+      go :: [Int] -> [Int] -> Int
       go diffs next
-        | all (== 0) next = accessor xs `combiner` foldFn (map accessor diffs)
+        | all (== 0) next = accessor xs `combiner` foldFn diffs
         | otherwise = let seqDifferences = differences next
-                      in go (seqDifferences : diffs) seqDifferences
+                      in go (accessor seqDifferences : diffs) seqDifferences
 
 part1 :: Input -> Int
-part1 = sum . map (last . extrapolateSequence Forward)
+part1 = sum . map (extrapolateSequence1 Forward)
 
 part2 :: Input -> Int
-part2 = sum . map (head . extrapolateSequence Back)
+part2 = sum . map (extrapolateSequence1 Back)
 
 day09 :: IO ()
 day09 = runSolution "09" parser part1 part2
