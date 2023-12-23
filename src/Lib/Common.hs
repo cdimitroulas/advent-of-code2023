a7cace18901829f15ffda701d2f53360b6b32831
cdimitroulas <c.dimitroulas@gmail.com>
2023-12-23 17:09:32 +0200

    Refactored day8

--- src/Lib/Common.hs
+++ src/Lib/Common.hs
@@ -1,7 +1,22 @@
-module Lib.Common (nTimes) where
+module Lib.Common (nTimes, whileM) where
+
+import Control.Monad (MonadPlus (mzero, mplus))
 
 nTimes :: Int -> (a -> a) -> a -> a
 nTimes n f = go n
   where
     go 0 = id
     go i = f . go (i - 1)
+
+-- Stolen this definition from Control.Monad.Loops
+whileM :: (Monad m) => m Bool -> m a -> m [a]
+whileM p f = go
+ where
+  go = do
+    x <- p
+    if x
+      then do
+        x' <- f
+        xs <- go
+        return (return x' `mplus` xs)
+      else return mzero
--- src/Solutions/Day08.hs
+++ src/Solutions/Day08.hs
@@ -1,18 +1,11 @@
 module Solutions.Day08 (day08, parser, part1, part2) where
 
-import Control.Monad (MonadPlus (..))
-import Control.Monad.ST
 import qualified Data.Attoparsec.Text as P
 import Data.Map.Lazy (Map)
 import qualified Data.Map.Strict as M
-import Data.STRef.Strict
 import qualified Data.Text as T
-import Debug.Trace
 import Lib.AOC (runSolution)
 import Lib.Parsing (linesOf, skipRestOfLine)
-import Lib.List (setAt, (!?))
-import GHC.Base (when)
-import Data.Maybe (isJust, catMaybes, fromJust)
 
 type Input = (String, Map String (String, String))
 
@@ -41,81 +34,23 @@ turn :: Char -> (String, String) -> String
 turn 'L' = fst
 turn 'R' = snd
 
--- Stolen this definition from Control.Monad.Loops
-whileM' :: (Monad m) => m Bool -> m a -> m [a]
-whileM' p f = go
- where
-  go = do
-    x <- p
-    if x
-      then do
-        x' <- f
-        xs <- go
-        return (return x' `mplus` xs)
-      else return mzero
+runPath :: Input -> String -> Int -> Int
+runPath _ node steps | last node == 'Z' = steps
+runPath input@(directions, mapLines) node0 steps =
+  let node1 = turn (directions !! steps) (mapLines M.! node0)
+  in  runPath input node1 (steps + 1)
 
 part1 :: Input -> Int
-part1 (directions, mapLines) = runST $ do
-  let directions' = cycle directions
-  stepCounterRef <- newSTRef 0
-  currentNodeRef <- newSTRef "AAA"
-
-  let isNotAtEnd = (/= "ZZZ") <$> readSTRef currentNodeRef
-
-  _ <- whileM' isNotAtEnd $ do
-    stepCounter <- readSTRef stepCounterRef
-    currentNode <- readSTRef currentNodeRef
-    writeSTRef currentNodeRef (turn (directions' !! stepCounter) (mapLines M.! currentNode))
-    modifySTRef stepCounterRef (+ 1)
-
-  readSTRef stepCounterRef
+part1 (directions, mapLines) = runPath (cycle directions, mapLines) "AAA" 0
 
 lcmFold :: [Integer] -> Integer
 lcmFold = foldl1 lcm
 
 part2 :: Input -> Integer
-part2 (directions, mapLines) = runST $ do
-  let directions' = cycle directions
-  let firstNodes = filter ((== 'A') . last) (M.keys mapLines)
-
-  stepCounterRef <- newSTRef 0
-  currentNodesRef <- newSTRef firstNodes
-  -- cyclesToEnd is used to track how often each path reaches an end node
-  -- we track the first time it reaches an end node and also the second time.
-  -- The difference between those two values is assumed to be cyclic.
-  --
-  -- Using the cycles for each path (we have 1 path per starting node) 
-  -- we can then calculate the Lowest Common Multiple which gives us the final answer
-  cyclesToEndRef <- newSTRef (replicate (length firstNodes) (Nothing, Nothing))
-
-  let hasCalculatedCycles = all (\(s, e) -> isJust s && isJust e) <$> readSTRef cyclesToEndRef
-
-  _ <- whileM' (not <$> hasCalculatedCycles) $ do
-    stepCounter <- readSTRef stepCounterRef
-    currentNodes <- readSTRef currentNodesRef
-
-    let nextDir = directions' !! fromInteger stepCounter
-    let nextNodes =
-          map
-            (\currentNode -> turn nextDir (mapLines M.! currentNode))
-            currentNodes
-
-    mapM_
-      ( \(node, idx) ->
-        when ((== 'Z') $ last node) $
-          modifySTRef cyclesToEndRef (\cycles -> case cycles !? idx of
-            Just (Just start, Nothing) -> setAt cycles idx (Just start, Just stepCounter)
-            Just (Nothing, Nothing) -> setAt cycles idx (Just stepCounter, Nothing)
-            _ -> cycles
-          )
-      )
-      (zip nextNodes [0 ..])
-
-    writeSTRef currentNodesRef nextNodes
-    modifySTRef stepCounterRef (+ 1)
-
-  cycles <- readSTRef cyclesToEndRef
-  return $ lcmFold (map (\(start, end) -> fromJust end - fromJust start) cycles)
+part2 (directions, mapLines) = lcmFold $ map runPath' firstNodes
+  where
+   firstNodes = filter ((== 'A') . last) (M.keys mapLines)
+   runPath' n = toInteger $ runPath (cycle directions, mapLines) n 0
 
 day08 :: IO ()
 day08 = runSolution "08" parser (fmap part1) (fmap part2)
